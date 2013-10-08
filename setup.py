26d8ee9b26a84bb86519c31252757ba85bc39f9d
makefu <github@syntax-fehler.de>
2013-10-08 20:02:36 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='relaxxapi',
     packages=['relaxxapi'],
-    version='0.11dev',
+    version='0.12dev',
     description='A Wrapper around the RelaxxPlayer MPD Web Interface',
     author='Felix Richter',
     license='WTFPL',
     author_email='pypi@syntax-fehler.de',
     url='https://github.com/makefu/relaxxapi',
-    download_url='https://github.com/makefu/relaxxapi/tarball/0.1',
+#    download_url='https://github.com/makefu/relaxxapi/tarball/0.1',
     keywords=['relaxxplayer', 'api'],
 )
