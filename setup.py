ecad48753a608ae4ab9d4e233f33987db4310396
makefu <github@syntax-fehler.de>
2013-10-08 20:10:41 +0200

    add new requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
     name='relaxxapi',
     packages=['relaxxapi'],
-    version='0.12dev',
+    version='0.13dev',
     description='A Wrapper around the RelaxxPlayer MPD Web Interface',
     author='Felix Richter',
     license='WTFPL',
     author_email='pypi@syntax-fehler.de',
     url='https://github.com/makefu/relaxxapi',
+    install_requires = ['requests'],
 #    download_url='https://github.com/makefu/relaxxapi/tarball/0.1',
     keywords=['relaxxplayer', 'api'],
 )
